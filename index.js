00922bc5ba7e6554aa690578b1f3c88b17d49bc0
WataruSugiura <38929316+flat35hdapp@users.noreply.github.com>
2019-11-27 00:03:52 +0900

    this is a test

--- index.js
+++ index.js
@@ -12,22 +12,24 @@ app.get("/mtg",function(req,res,next){
 
 /* sample.js */
 'use strict'
-const mysql = require('mysql');
-async function mySqlSet(){
-  // Connectionを定義する
-  const connection = await mysql.createConnection({
-      host: process.env.MYSQL_HOST,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PW,
-      database: process.env.MYSQL_DB,
+function mySqlSet(){
+  const mysql = require('mysql');
+  const connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'nodeuser',
+      password: 'nodeuser',
+      database: 'memberlist',
   });
+
+  // Connectionを定義する
+  connection.connect();
+
   let sql = 'select * from memberlist.user';
-  await connection.connect();
-  await connection.query(sql, (err, rows, fields) => {
+  connection.query(sql, (err, rows, fields) => {
       if (err) throw err;
       console.log('userテーブル: ', rows);
   });
-  await connection.end();
-}
 
+  connection.end();
+}
 mySqlSet();

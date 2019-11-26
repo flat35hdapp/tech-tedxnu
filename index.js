1528c7f05be2630b154dbff9ebb563c8dc9763e7
WataruSugiura <38929316+flat35hdapp@users.noreply.github.com>
2019-11-26 16:06:45 +0900

    mysql
    mysql接続できるように。

---
+++ index.js
@@ -0,0 +1,33 @@
+/*const express = require("express");
+const app = express();
+
+const server = app.listen(3000, function(){
+  console.log("Node.js listens to PORT: " + server.address().port);
+});
+
+app.get("/mtg",function(req,res,next){
+  res.json();
+})
+*/
+
+/* sample.js */
+'use strict'
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'nodeuser',
+    password: 'nodeuser',
+    database: 'memberlist',
+});
+
+// Connectionを定義する
+connection.connect();
+
+let sql = 'select * from memberlist.user';
+connection.query(sql, (err, rows, fields) => {
+    if (err) throw err;
+    console.log('userテーブル: ', rows);
+});
+
+connection.end();

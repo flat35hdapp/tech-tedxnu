8914906594a96a7a36c3bcab58b11d6e5e9ad829
WataruSugiura <38929316+flat35hdapp@users.noreply.github.com>
2019-12-16 18:37:32 +0900

    Revert "mongodb: insert_mtg&find_user"
    This reverts commit c7091c93d43d80e95e24da702ba92ce39d057862.

--- routes/mongodb/slack.js
+++ routes/mongodb/slack.js
@@ -4,31 +4,12 @@ const mongoPort = process.env.MONGODB_PORT;
 const dbName = "";
 const hostUrl = "mongodb://localhost:" + mongoPort + "/";
 
-//初期設定欄
-const users_collection = "users";
-const mtgs_collection = "mtgs";
-
-const find_user = async (user_id) => {
+const user = async (user_id) => {
+  const collectionName = users;
   const client = await mongoClient.connect(hostUrl);
   const db = await client.db(dbName);
-  const collection = db.collection(users_collection);
-  const docs = await collection.find({'slack_user_id':user_id}).toArray();
-  client.close();
-  return docs[0];
-}
-
-const insert_mtg = async (mtg_obj) => {//書きかけ注意！！！
-  try{
-    const client = await mongoClient.connect(hostUrl);
-    const db = await client.db(dbName);
-    const collection = db.collection(mtgs_collection);
-    await collection.insert(mtg_obj);
-    return new promise();
-  }catch(err){
-    return new error();
-  }finally{
-    client.close();
-  }
+  const collection = db.collection(users);
+  await collection.
 
 }
-module.exports = {find_user};
+module.exports = {user};
